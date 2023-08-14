80844d2e3fbaa53a3f8d764d151090fc96df93f3
SAMUDRAgg <141972778+SAMUDRAgg@users.noreply.github.com>
2023-08-14 06:00:10 +0530

    Added solution - LeetHub

---
+++ Remove Duplicates from unsorted array - GFG/remove-duplicates-from-unsorted-array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    vector<int> removeDuplicate(int A[], int N) {
+           unordered_map<int,int>mp;
+        for(int i=0;i<N;i++)
+        {
+            mp[A[i]]++;
+        }
+        vector<int>ans;
+        for(int i=0;i<N;i++)
+        {
+            if(mp[A[i]]>=1)
+            {
+                ans.push_back(A[i]);
+                mp[A[i]]=0;
+            }
+        }
+        
+            
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver method to test above method
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        vector<int> v = ob.removeDuplicate(a, n);
+
+        for (auto it : v) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
