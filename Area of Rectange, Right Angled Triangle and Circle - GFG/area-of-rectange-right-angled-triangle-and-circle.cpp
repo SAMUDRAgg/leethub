0501428e087bc72234510d33e15d415523dd7292
SAMUDRAgg <141972778+SAMUDRAgg@users.noreply.github.com>
2023-08-17 01:34:56 +0530

    Added solution - LeetHub

---
+++ Area of Rectange, Right Angled Triangle and Circle - GFG/area-of-rectange-right-angled-triangle-and-circle.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> getAreas(int L , int W , int B , int H , int R) {
+        // code here
+        vector<int>v;
+    
+    int x=L*W;
+    int y=(B*H)/2;
+    int z=3.14*R*R;
+    
+    v.push_back(x);
+    v.push_back(y);
+    v.push_back(z);
+    
+    return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int L,W,B,H,R;
+        
+        cin>>L>>W>>B>>H>>R;
+        
+        Solution ob;
+        vector<int> ans = ob.getAreas(L,W,B,H,R);
+        cout<<ans[0]<<" "<<ans[1]<<" "<<ans[2]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
