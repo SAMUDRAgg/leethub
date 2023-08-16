e8ea2f9172acf7e8239e6e4cdeab4cdba845fcfd
SAMUDRAgg <141972778+SAMUDRAgg@users.noreply.github.com>
2023-08-17 01:00:38 +0530

    Added solution - LeetHub

---
+++ Rohan's Love for Matrix - GFG/rohans-love-for-matrix.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution {
+  public:
+    int firstElement(int n) 
+    {
+           int mod=1e9+7;
+        if(n==1 ||  n==2){return 1;}
+        int a=1,b=1,c;
+        for(int i=3;i<=n;i++){
+            c=(a+b)%mod;
+            a=b;
+            b=c;
+        }
+        return c%mod;
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int  n;
+        cin >> n ;
+        Solution ob;
+        cout<<ob.firstElement(n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
