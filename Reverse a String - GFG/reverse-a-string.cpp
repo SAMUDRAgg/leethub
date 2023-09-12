afa2bb4e9805efdcf27efa9729de7a47632498a9
SAMUDRAgg <141972778+SAMUDRAgg@users.noreply.github.com>
2023-09-12 21:54:56 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -18,7 +18,9 @@ class Solution
         int e=str.length()-1;
         while(s<=e){
             swap(str[s],str[e]);
-            s++;e--;
+            s++;
+            e--;
+            
         }
         return str;
     }
