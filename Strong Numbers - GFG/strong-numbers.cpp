e2ed3e53d6dfdef54761cf69e23ad53b99ed3a81
SAMUDRAgg <141972778+SAMUDRAgg@users.noreply.github.com>
2023-09-15 03:39:08 +0530

    Added solution - LeetHub

--- Strong Numbers - GFG/strong-numbers.cpp
+++ Strong Numbers - GFG/strong-numbers.cpp
@@ -5,32 +5,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int fac(int N){
-      int f=1;
-      for(int i=1;i<=N;++i){
-          f*=i;
-      }
-      return f;
-  }
-    int isStrong(int N) {
-       int num=N;
-      int sum=0;
-       int rem=0;
-       while(N!=0){
-       rem=N%10;
-      sum+=fac(rem);
-       N=N/10;
-           
+   long long int fact(int N){
+      long long  int pro=1;
+       for(int i=1;i<=N;i++){
+           pro*=i;
        }
-      if(sum==num){
-          return 1;
-          
-      }else{
-          return 0;
-      }
-           
-       
-       
+       return pro;
+   }
+    int isStrong(int N) {
+        int rem=0;
+        int sum=0;
+        int num=N;
+        while(N!=0){
+            rem=N%10;
+            sum+=fact(rem);
+            N=N/10;
+        }
+        if(num==sum){
+            return 1;
+        }else{
+            return 0;
+        }
     }
 };
 
