771c4189143195571356a2713f8aa7409181f12e
SAMUDRAgg <141972778+SAMUDRAgg@users.noreply.github.com>
2023-09-15 04:25:10 +0530

    Added solution - LeetHub

--- Rohan's Love for Matrix - GFG/rohans-love-for-matrix.cpp
+++ Rohan's Love for Matrix - GFG/rohans-love-for-matrix.cpp
@@ -9,8 +9,9 @@ class Solution {
   public:
     int firstElement(int n) 
     {
-           int mod=1e9+7;
-        if(n==1 ||  n==2){return 1;}
+            int mod=1e9+7;
+        if(n==1 ||  n==2){
+            return 1;}
         int a=1,b=1,c;
         for(int i=3;i<=n;i++){
             c=(a+b)%mod;
