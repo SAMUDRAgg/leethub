4ae81f9438d9d577778e41cbecbd0a4ea5a71b21
SAMUDRAgg <141972778+SAMUDRAgg@users.noreply.github.com>
2023-09-14 22:36:46 +0530

    Added solution - LeetHub

--- Sorting Elements of an Array by Frequency - GFG/sorting-elements-of-an-array-by-frequency.cpp
+++ Sorting Elements of an Array by Frequency - GFG/sorting-elements-of-an-array-by-frequency.cpp
@@ -1,52 +1,41 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
-#include<map>
-using namespace std;
-void sortt(vector<int> &arr){
-    
-   
-    map<int,int> mp;
-    for(int i=0;i<arr.size();i++){
-        mp[arr[i]]++;
-        
-    }
-     sort(arr.begin(),arr.end(),[&](int a,int b){
-         return mp[a]!=mp[b]? mp[a]>mp[b]:a<b;
-     });
-    
- 
-}
+#include<algorithm>
+#include<unordered_map>
 
+using namespace std;
+
+void sot(vector<int>&arr){
+  unordered_map<int,int> mp;
+  for(int i=0;i<arr.size();i++){
+      mp[arr[i]]++;
+  }
+  
+  sort(arr.begin(),arr.end(),[&] (int a,int b){
+      return mp[a]!=mp[b]? mp[a]>mp[b]:a<b;
+  });
+  
+  
+}
 
 int main()
  {
-	
-	int t;cin>>t;
-	while(t--){
-	    int n;cin>>n;
-	    vector<int> arr;
-	    int a;
-	    for(int i=0;i<n;i++){
-	        cin>>a;
-	        arr.push_back(a);
-	        
-	    }
-	    
-	
-	
-	sortt(arr);
-
-for(auto&it:arr){
-    cout<<it<<" ";
-}
-
-cout<<endl;	
-	
-	}
-	
-	
-	
-	
+     int t;cin>>t;
+     while(t--){
+         int n;cin>>n; int a;
+         vector<int> arr;
+         for(int i=0;i<n;i++){
+             cin>>a;
+             arr.push_back(a);
+         }
+         
+     
+   sot(arr);
+   
+   for(auto&it:arr){
+       cout<<it<<" ";
+   }
+   cout<<endl;
+     }
 	return 0;
 }
