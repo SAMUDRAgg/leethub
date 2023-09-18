6fec7d376936e70ff39c405e746db98880696cbf
SAMUDRA GANTAIT <141972778+SAMUDRAgg@users.noreply.github.com>
2023-09-18 05:43:18 +0530

    Added solution - LeetHub

--- Sum of numbers in string - GFG/sum-of-numbers-in-string.cpp
+++ Sum of numbers in string - GFG/sum-of-numbers-in-string.cpp
@@ -14,21 +14,20 @@ class Solution
     //Function to calculate sum of all numbers present in a string.
     int findSum(string str)
     {
-    int num=0;
-    int sum=0;
-    for(int i=0;i<str.length();i++){
-        if(str[i]>='0'&&str[i]<='9'){
-            num=num*10+str[i]-'0';
-            
-        }else{
-            if(num!=0){
-                sum=sum+num;
-                num=0;
-            }
-        }
-    }
-    return sum+num;
     	
+       int num=0;
+       int sum=0;
+       for(int i=0;i<str.length();i++){
+           if(str[i]>='0'&&str[i]<='9'){
+               num=num*10+str[i]-'0';
+           }else{
+               if(num!=0){
+                   sum=num+sum;
+                   num=0;
+               }
+           }
+       }
+    	return sum+num;
     }
 };
 
