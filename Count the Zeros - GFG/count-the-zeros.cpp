0e36ef95397d60153fcc10122ffafbf35eed9509
SAMUDRA GANTAIT <141972778+SAMUDRAgg@users.noreply.github.com>
2023-09-19 10:33:31 +0530

    Added solution - LeetHub

--- Count the Zeros - GFG/count-the-zeros.cpp
+++ Count the Zeros - GFG/count-the-zeros.cpp
@@ -10,16 +10,15 @@ using namespace std;
 class Solution{   
 public:
     int countZeroes(int arr[], int n) {
-        int count =0;
-      for(int i=0;i<n;i++){
-          if(0==arr[i]){
-              count++;
-          }
-      }
-      return count;
+       int count =0;
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]==0){
+                count++;
+            }
+        }
+        return count++;
     }
 };
-
 
 //{ Driver Code Starts.
 int main() {
