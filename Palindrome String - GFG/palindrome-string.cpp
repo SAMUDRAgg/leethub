f674f43580a0114519521c4554d0c9ed6f772d7f
SAMUDRA GANTAIT <141972778+SAMUDRAgg@users.noreply.github.com>
2023-10-07 01:05:52 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.cpp
+++ Palindrome String - GFG/palindrome-string.cpp
@@ -8,19 +8,28 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-	
+	char a(char ch){
+	    char temp;
+	    if(ch>='a'&&ch<='z'){
+	        return ch;
+	    }else{
+	        temp=ch-'A'+'a';
+	    }
+	    return temp;
+	}
 	
 	int isPalindrome(string S)
 	{
-           int s=0;
-           int e=S.size()-1;
-           while(s<=e){
-               if(S[s]!=S[e]){
-                   return 0;
-               }
-               s++;e--;
-           }
-           return 1;
+	    int n=S.length()-1;
+	    int i=0;
+	    while(i<n){
+	        if(a(S[i])==a(S[n])){
+	            i++;n--;
+	        }else{
+	            return 0;
+	        }
+	    }
+	    return 1;
 	}
 
 };
