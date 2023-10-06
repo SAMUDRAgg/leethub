0cb95870a79eaf3b21f87fab3a7929973f4dc78d
SAMUDRA GANTAIT <141972778+SAMUDRAgg@users.noreply.github.com>
2023-10-07 00:21:48 +0530

    Added solution - LeetHub

--- Maximum Product Subarray - GFG/maximum-product-subarray.cpp
+++ Maximum Product Subarray - GFG/maximum-product-subarray.cpp
@@ -10,17 +10,17 @@ public:
 
 	// Function to find maximum product subarray
 	long long maxProduct(vector<int> arr, int n) {
-	    long long int  pre=1;
-	    long long int suf=1; long long int maxi=INT_MIN;
-	    for(int i=0;i<arr.size();i++){
-	        if(pre==0) pre=1;
-	        if(suf==0) suf=1;
-	        pre*=arr[i];
-	        suf*=arr[n-1-i];
-	        maxi=max(maxi,max(pre,suf));
-	        
-	    }
-	    return maxi;
+	   long long int pre=1;
+	   long long int suf=1;
+	  long long  int maxi=INT_MIN;
+	   for(int i=0;i<n;i++){
+	       if(pre==0) pre=1;
+	       if(suf==0) suf =1;
+	       pre*=arr[i];
+	       suf*=arr[n-1-i];
+	       maxi=max(maxi,max(pre,suf));
+	   }
+	   return maxi;
 	}
 };
 
