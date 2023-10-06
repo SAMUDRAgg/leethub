76e91e9e0d005574e021ab004492e8ab159bee47
SAMUDRA GANTAIT <141972778+SAMUDRAgg@users.noreply.github.com>
2023-10-07 01:24:17 +0530

    Added solution - LeetHub

--- Remove all characters other than alphabets - GFG/remove-all-characters-other-than-alphabets.cpp
+++ Remove all characters other than alphabets - GFG/remove-all-characters-other-than-alphabets.cpp
@@ -9,17 +9,16 @@ using namespace std;
 class Solution{
 public:
     string removeSpecialCharacter(string s) {
-        string ans=""; 
-        for(int i=0;i<s.length();i++){
-            if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')){
-                ans.push_back(s[i]);
-            }
-        }
-        if(ans.length()==0){
-            return "-1";
-        }
-        return ans;
-            
+      string ans;
+      for(int i=0;i<s.length();i++){
+          if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')){
+              ans.push_back(s[i]);
+          }
+      }
+      if(ans.length()==0){
+          return "-1";
+      }
+      return ans;
     }
 };
 
