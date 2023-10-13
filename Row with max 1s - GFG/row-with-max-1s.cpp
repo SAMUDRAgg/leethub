cbeeadd52d0afb037197963ff88aded63b401c95
SAMUDRA GANTAIT <141972778+SAMUDRAgg@users.noreply.github.com>
2023-10-13 10:03:43 +0530

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.cpp
+++ Row with max 1s - GFG/row-with-max-1s.cpp
@@ -7,22 +7,14 @@ using namespace std;
 class Solution{
 public:
 	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
-	    int r=-1;
-	    int c=m-1;
-	    for(int i=0;i<n;i++){
-	        for(int j=c;j>=0;j--){
-	            if(arr[i][j]==1){
-	                r=i;
-	                
-	                c--;
-	                
-	            }
-	            else{
-	              break;
-	            }
+	   for(int i=0; i<m; i++) {
+	        for(int j=0; j<n; j++){
+	            if(arr[j][i] == 1)
+	                return j;
 	        }
 	    }
-	    return r;
+	    
+	    return -1;
 	}
 
 };
