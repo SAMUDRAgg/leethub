cde53dfafcd730cdfd48b5efb1fda3132ccef878
SAMUDRA GANTAIT <141972778+SAMUDRAgg@users.noreply.github.com>
2023-10-26 00:12:21 +0530

    Added solution - LeetHub

---
+++ Buildings receiving sunlight - GFG/buildings-receiving-sunlight.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    int longest(int arr[],int n)
+    {
+      int count =1; int l=arr[0];
+      for(int i=1;i<n;i++){
+          if(arr[i]>=l){
+         
+              count++;
+               l=arr[i];
+          
+          }
+          
+      }
+      return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int i, arr[n+5];
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        Solution ob;
+        cout<<ob.longest( arr, n )<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
