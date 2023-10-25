257f3c2bc9cf6323549c7ae1bcbe6fdeb678055c
SAMUDRA GANTAIT <141972778+SAMUDRAgg@users.noreply.github.com>
2023-10-25 22:54:36 +0530

    Added solution - LeetHub

--- Palindromic Array - GFG/palindromic-array.cpp
+++ Palindromic Array - GFG/palindromic-array.cpp
@@ -7,48 +7,36 @@ using namespace std;
 // } Driver Code Ends
 /*Complete the function below*/
 #include<vector>
+
 class Solution {
 public:
-
-int check( int n){
-    vector<int> a;
-    int rem=0;
-    while(n!=0){
-        rem=n%10;
-        a.push_back(rem);
-        n=n/10;
-    }
-    
-       int s=0;
-    	int e=a.size()-1;
-    	while(s<e){
-    	    if(a[s]==a[e]){
-    	        s++;
-    	        e--;
-    	    }
-    	    else{
+     bool pal(int num){
+         int rem;
+         vector<int> v;
+         while(num!=0){
+             rem=num%10;
+             v.push_back(rem);
+             num=num/10;
+         }
+         int s=0;
+         int e=v.size()-1;
+         while(s<=e){
+             if(v[s]!=v[e]){
+                 return 0;
+             }
+             s++;
+             e--;
+         }
+         return 1;
+     }
+    int PalinArray(int a[], int n)
+    {
+    	for(int i=0;i<n;i++){
+    	    if(pal(a[i])==0){
     	        return 0;
     	    }
     	}
     	return 1;
-}
-
-
-    int PalinArray(int a[], int n)
-    {
-        int count =0;
-        
-        for(int i=0;i<n;i++){
-            if(check(a[i])!=1){
-                count++;
-            }
-        }
-        if(count>0){
-            return 0;
-        }else{
-           return 1; 
-        }
-        
     }
 };
 
