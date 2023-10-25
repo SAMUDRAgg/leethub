1560376feae53f6fa5e605ef5e099319d7449c7c
SAMUDRA GANTAIT <141972778+SAMUDRAgg@users.noreply.github.com>
2023-10-25 23:28:15 +0530

    Added solution - LeetHub

--- Element with left side smaller and right side greater - GFG/element-with-left-side-smaller-and-right-side-greater.cpp
+++ Element with left side smaller and right side greater - GFG/element-with-left-side-smaller-and-right-side-greater.cpp
@@ -24,29 +24,31 @@ int main() {
 
 
 
+
+
 int findElement(int arr[], int n) {
-    int lLargest[n];
-    int rSmallest[n];
-    lLargest[0] = arr[0];
-    rSmallest[n-1] = arr[n-1];
+    int mini[n];
+    int maxi[n];
+    maxi[0] = arr[0];
+    mini[n-1] = arr[n-1];
     
     int l = arr[0];
     for (int i = 1; i<n; i++){
         if (arr[i]>l)
             l = arr[i];
-        lLargest[i] = l;
+        maxi[i] = l;
     }
     
     int s = arr[n-1];
     for (int i = n-2; i>=0; i--){
         if (arr[i]<s)
             s = arr[i];
-        rSmallest[i] = s;
+        mini[i] = s;
     }
     
      for(int i=1;i<n-1;i++){
-        if( lLargest[i]==rSmallest[i]){
-            return rSmallest[i];
+        if( mini[i]==maxi[i]){
+            return mini[i];
         }
     }
     return -1;
